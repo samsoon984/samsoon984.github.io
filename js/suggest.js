b46649281ddc658882fd09aaf1eed9d6dcf0491c
samsoon984 <entks82137@naver.com>
2020-11-07 07:20:17 +0900

    alert

--- js/suggest.js
+++ js/suggest.js
@@ -1,15 +1,7 @@
 function submitbtn(){
-    var useremail = document.getElementById('email').value;
-    var emailcheck = useremail.includes('@' && '.');
-    if (emailcheck){
-        var d = new Date();
-        var newfile = CreateObject("Scripting.FileSystemObject"); 
-        var s = newfile.CreateTextFile(`../suggest/${d}.txt`);
-        s.writeline(useremail);
-        s.writeline(document.getElementById('inputsuggest').value);
-        alert('제출되었습니다!!');
-
-    } else{
-        alert('올바른 이메일을 입력해주세요!!')
-    }
+    var getEmail = document.getElementById('email')
+    getEmail.select();
+    document.execCommand("Copy");
+    alert(`${getEmail.value} 복사 완료!`)
+    document.execCommand("copy");
 }
