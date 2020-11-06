372d304db019d3bd21288b614b730c13b8806180
samsoon984 <entks82137@naver.com>
2020-11-06 09:41:52 +0900

    sort,,,

--- js/main.js
+++ js/main.js
@@ -492,7 +492,7 @@ function load_data(){
     document.getElementById("85마릿수2").innerText = calcmonster;
     document.getElementById("85경험치").innerText = calcExpPerHour;
     document.getElementById("85메소").innerText = calcMoney;
-    var amountOfmonsters = 40;
+    var amountOfmonsters = 39;
     var exp_of_monster = 594040;
     var level_of_monster = 251;
     go_calc(amountOfmonsters, exp_of_monster, level_of_monster);
@@ -774,10 +774,65 @@ function load_data(){
 
 
 }
+function removeComma(str){
+    n = parseInt(str.replace(/,/g,""));
+	return n;
+}
 
-
-
-
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("myTable");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /* Check if the two rows should switch place,
+        based on the direction, asc or desc: */
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount ++;
+      } else {
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
 calcbutton.addEventListener('click', function(){
     var spawn_monsters = document.getElementById('spawn_monsters').value;
     var exp_of_monster = document.getElementById('exp_of_monster').value;
